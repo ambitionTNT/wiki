ed2571fc8e15492f707e88c0ffa5c15304499374
ambition <1292217724@qq.com>
2023-09-23 14:47:18 +0800

    2-5 修改启动文案

--- src/main/java/edu/scnu/wiki/WikiApplication.java
+++ src/main/java/edu/scnu/wiki/WikiApplication.java
@@ -1,13 +1,25 @@
 package edu.scnu.wiki;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+import java.util.logging.Logger;
 
 @SpringBootApplication
+@Slf4j
 public class WikiApplication {
+/*
+    private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);*/
 
     public static void main(String[] args) {
-        SpringApplication.run(WikiApplication.class, args);
+
+        SpringApplication application = new SpringApplication(WikiApplication.class);
+        ConfigurableEnvironment environment = application.run(args).getEnvironment();
+        log.info("启动成功");
+        log.info("地址：\thttp://127.0.0.1:{}", environment.getProperty("server.port"));
     }
 
 }

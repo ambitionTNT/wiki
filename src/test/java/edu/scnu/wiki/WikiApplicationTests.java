c1c162c31b53b75ddf0f3823166cefb9d8d4777b
ambition <1292217724@qq.com>
2023-09-23 10:46:28 +0800

    2-2 init

---
+++ src/test/java/edu/scnu/wiki/WikiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.scnu.wiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

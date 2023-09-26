2f6d8629b7a851e352451537249e1eba90a96c3c
ambition <1292217724@qq.com>
2023-09-26 21:17:35 +0800

    6-6 继承pageHelper，完成分页操作

--- src/main/java/edu/scnu/wiki/service/impl/EbookServiceImpl.java
+++ src/main/java/edu/scnu/wiki/service/impl/EbookServiceImpl.java
@@ -1,5 +1,7 @@
 package edu.scnu.wiki.service.impl;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import edu.scnu.wiki.domain.Ebook;
 import edu.scnu.wiki.domain.EbookExample;
 import edu.scnu.wiki.mapper.EbookMapper;
@@ -7,6 +9,7 @@ import edu.scnu.wiki.req.EbookReq;
 import edu.scnu.wiki.resp.EbookResp;
 import edu.scnu.wiki.service.EbookService;
 import edu.scnu.wiki.utils.CopyUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -24,6 +27,7 @@ import java.util.List;
  * @date 2023/9/24 16:44
  */
 @Service
+@Slf4j
 public class EbookServiceImpl implements EbookService {
 
     @Autowired
@@ -35,9 +39,11 @@ public class EbookServiceImpl implements EbookService {
         if (!ObjectUtils.isEmpty(req.getName())){
             criteria.andNameLike("%" + req.getName() + "%");
         }
-
+        PageHelper.startPage(1,3);
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
-
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        log.info("总行数：",pageInfo.getTotal());
+        log.info("总行数：",pageInfo.getPages());
         List<EbookResp> respList = CopyUtil.copyList(ebookList, EbookResp.class);
 
         return respList;

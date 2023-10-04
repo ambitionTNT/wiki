8d609625aa7e64e7680ac82422edf4cc93fe06cf
ambition <1292217724@qq.com>
2023-10-04 21:34:03 +0800

    9-1 用户后端

---
+++ src/main/java/edu/scnu/wiki/mapper/UserMapper.java
@@ -0,0 +1,33 @@
+package edu.scnu.wiki.mapper;
+
+import edu.scnu.wiki.domain.User;
+import edu.scnu.wiki.domain.UserExample;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface UserMapper {
+    long countByExample(UserExample example);
+
+    int deleteByExample(UserExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(User row);
+
+    int insertSelective(User row);
+
+    List<User> selectByExample(UserExample example);
+
+    User selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("row") User row, @Param("example") UserExample example);
+
+    int updateByExample(@Param("row") User row, @Param("example") UserExample example);
+
+    int updateByPrimaryKeySelective(User row);
+
+    int updateByPrimaryKey(User row);
+}
